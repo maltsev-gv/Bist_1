6213c58361dec0a3ac126f6729384e7b2a1d7cb8
Мальцев Геннадий Владимирович <svarogichred@yandex.ru>
2020-09-27 22:28:57 +0700

    Initial commit

---
+++ Bist_1/Bist_1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bist_1.ViewModels;
+using Xamarin.Forms;
+
+namespace Bist_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            var context = ((Button) sender).BindingContext;
+            var viewModel = context as MainViewModel;
+            var rad = viewModel?.Radius;
+            if (viewModel != null)
+            {
+                viewModel.Radius = rad == 30
+                    ? 100
+                    : rad == 100
+                        ? 50
+                        : 30;
+                viewModel.ColorName = viewModel.ColorName == "Aqua" ? "Magenta" : "Aqua";
+            }
+        }
+    }
+}
---
+++ Bist_1/Bist_1/ViewModels/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Bist_1.Annotations;
+using Rcn.Common;
+
+namespace Bist_1.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            //object obj = new object();
+
+            //double x = 5.56e12;
+            //var t = x.ToString();
+            ////obj = Radius;
+            //Radius = 2;
+            //MyMethod();
+
+        }
+
+        [InitialValue("LightGreen")]
+        public string ColorName
+        {
+            get => GetVal<string>();
+            set => SetVal(value);
+        }
+
+
+        public int BasicRadius => 10;
+
+        public double Radius
+        {
+            get
+            {
+                SetInitialVal((double)BasicRadius);
+                return GetVal<double>();
+            }
+            set
+            {
+                SetVal(value);
+                var classString = this.ToString();
+            }
+        }
+
+        public void MyMethod()
+        { 
+            RaisePropertyChanged();
+        }
+
+        //private double _radius = 30;
+        //public double Radius
+        //{
+        //    get => _radius;
+        //    set
+        //    {
+        //        _radius = value;
+        //        OnPropertyChanged(nameof(Radius));
+        //    }
+        //}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
